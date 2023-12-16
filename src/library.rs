3fdc856617f4d51eee281d31877a182cbb594037
Luke Lynch <luke@lukelynch.io>
2023-12-17 00:30:41 +1100

    forgot to save my latest changes

--- src/library.rs
+++ src/library.rs
@@ -63,12 +63,48 @@ impl Library {
         }
     }
     
-    fn search_library_for_book_by_int(&self, book_id: u32) -> Option<usize> {
+    pub fn search_library_for_book_by_int(&self, book_id: u32) -> Option<usize> {
         for i in 0..self.books.len() {
             if self.books[i].id == book_id {
                 return Some(i);
             }
         }
         None
+    }
+
+    pub fn return_book_by_book_id(&mut self, book_id: usize) {
+        let book_to_return: &mut Book = self.books.get_mut(book_id).unwrap();
+        match &book_to_return.is_available {
+            BorrowStatus::Available => {
+                println!("Book: {} is already available", &book_to_return.name);
+            }
+            BorrowStatus::Borrowed { borrower} => {
+                println!("{} successfully returned: {}!", &borrower, book_to_return.name);
+                book_to_return.is_available = BorrowStatus::Available;
+            }
+        }
+    }
+
+    pub fn search_for_books_borrowed_by_borrower(&self, borrower_name: &String) -> Vec<&Book> {
+        let mut books_borrowed_by_borrower: Vec<&Book> = Vec::new();
+        for book in &self.books {
+            match &book.is_available {
+                BorrowStatus::Borrowed { borrower } => {
+                    if borrower == borrower_name {
+                        books_borrowed_by_borrower.push(book);
+                    }
+                }
+                _ => ()
+            }
+        }
+        books_borrowed_by_borrower
+    }
+
+    pub fn print_books_borrowed_by_borrower(&self, borrower_name: &String) {
+        let books_borrowed_by_borrower = self.search_for_books_borrowed_by_borrower(borrower_name);
+        println!("Books borrowed by {}:", borrower_name);
+        for book in books_borrowed_by_borrower {
+            println!("{} - {}", book.id, book.name);
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,25 @@ fn main() {
                     break 'program;
                 }
             },
-            MenuOptions::Return => println!("Sorry, the library has not implemented this feature yet"),
+            MenuOptions::Return => {
+                let books_borrowed_by_user = library.search_for_books_borrowed_by_borrower(&username);
+                if books_borrowed_by_user.is_empty() {
+                    println!("You have no books to return!");
+                    continue;
+                }
+                library.print_books_borrowed_by_borrower(&username);
+                let mut return_book_input = String::new();
+                print!("Please enter the ID of the book you would like to return: ");
+                io::stdout().flush().unwrap();
+                std::io::stdin().read_line(&mut return_book_input).unwrap();
+                if let Ok(book_id) = return_book_input.trim().parse::<u32>() {
+                    let book_index = library.search_library_for_book_by_int(book_id).unwrap();
+                    library.return_book_by_book_id(book_index);
+                } else {
+                    println!("Invalid input!");
+                    continue;
+                }
+            },
             MenuOptions::Quit => break,
         }
     }
